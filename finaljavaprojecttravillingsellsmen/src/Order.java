47162d99c6f338d0a73a06279b6ba701592d4ba4
yasiru2077 <yasiruudana1999@gmail.com>
2023-09-25 01:11:14 +0530

    update

---
+++ finaljavaprojecttravillingsellsmen/src/Order.java
@@ -0,0 +1,17 @@
+public class Order {
+    private int id;
+    private Customer customer;
+
+    public Order(int id, Customer customer) {
+        this.id = id;
+        this.customer = customer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+}
